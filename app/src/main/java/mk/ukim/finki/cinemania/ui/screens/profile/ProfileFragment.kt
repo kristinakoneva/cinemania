b86f41d50898d7a0ba9a0c73882dee25a32c0fc3
Kristina Koneva <kristina.koneva@infinum.com>
2024-03-30 19:14:10 +0100

    Populate profile screen with some data

--- app/src/main/java/mk/ukim/finki/cinemania/ui/screens/profile/ProfileFragment.kt
+++ app/src/main/java/mk/ukim/finki/cinemania/ui/screens/profile/ProfileFragment.kt
@@ -1,12 +1,21 @@
 package mk.ukim.finki.cinemania.ui.screens.profile
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.coroutineScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import mk.ukim.finki.cinemania.R
 import mk.ukim.finki.cinemania.databinding.FragmentProfileBinding
 import mk.ukim.finki.cinemania.extensions.viewBinding
+import mk.ukim.finki.cinemania.ui.screens.adapters.MovieAdapter
 import mk.ukim.finki.cinemania.ui.shared.LoadingDialog
+import mk.ukim.finki.cinemania.utils.Constants
 
 @AndroidEntryPoint
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
@@ -16,4 +25,72 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
     private val viewModel by viewModels<ProfileViewModel>()
 
     private var loadingDialog: LoadingDialog? = null
+
+    private lateinit var watchedMovieRecommendationsAdapter: MovieAdapter
+
+    private lateinit var likedMovieRecommendationsAdapter: MovieAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initAdapters()
+        super.onViewCreated(view, savedInstanceState)
+        collectStateFlow()
+    }
+
+    private fun initAdapters() = with(binding) {
+        watchedMovieRecommendationsAdapter = MovieAdapter(items = emptyList(), areActionsVisible = false) { movie ->
+            Toast.makeText(requireContext(), "Clicked on " + movie.title, Toast.LENGTH_SHORT).show()
+        }
+        watchedMovieRecommendationsRecyclerView.adapter = watchedMovieRecommendationsAdapter
+
+        likedMovieRecommendationsAdapter = MovieAdapter(items = emptyList(), areActionsVisible = false) { movie ->
+            Toast.makeText(requireContext(), "Clicked on " + movie.title, Toast.LENGTH_SHORT).show()
+        }
+        likedMovieRecommendationsRecyclerView.adapter = likedMovieRecommendationsAdapter
+    }
+
+    private fun collectStateFlow() {
+        viewLifecycleOwner.lifecycle.coroutineScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.stateFlow.collect { state ->
+                    state?.let {
+                        with(binding) {
+                            name.text = state.name
+                            if (state.watchedMovieRecommendations.isNotEmpty()) {
+                                watchedMovieRecommendationsAdapter.submitList(state.watchedMovieRecommendations)
+                                watchedMovieRecommendationsRecyclerView.visibility = View.VISIBLE
+                                becauseYouWatchedTitle.visibility = View.VISIBLE
+                                becauseYouWatchedTitle.text = getString(R.string.because_you_watched, state.watchedMovieRecommendationsName)
+                            } else {
+                                watchedMovieRecommendationsRecyclerView.visibility = View.GONE
+                                becauseYouWatchedTitle.visibility = View.GONE
+                            }
+                            if (state.likedMovieRecommendations.isNotEmpty()) {
+                                likedMovieRecommendationsAdapter.submitList(state.likedMovieRecommendations)
+                                likedMovieRecommendationsRecyclerView.visibility = View.VISIBLE
+                                becauseYouLikedTitle.visibility = View.VISIBLE
+                                becauseYouLikedTitle.text = getString(R.string.because_you_liked, state.likedMovieRecommendationsName)
+                            } else {
+                                likedMovieRecommendationsRecyclerView.visibility = View.GONE
+                                becauseYouLikedTitle.visibility = View.GONE
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycle.coroutineScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.loadingStateFlow.collect { isLoading ->
+                    if (isLoading && loadingDialog == null && childFragmentManager.findFragmentByTag(Constants.LOADING_DIALOG_TAG) == null) {
+                        loadingDialog = LoadingDialog()
+                        loadingDialog?.show(childFragmentManager, Constants.LOADING_DIALOG_TAG)
+                    } else {
+                        loadingDialog?.dismiss()
+                        loadingDialog = null
+                    }
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/mk/ukim/finki/cinemania/ui/screens/profile/ProfileViewModel.kt
+++ app/src/main/java/mk/ukim/finki/cinemania/ui/screens/profile/ProfileViewModel.kt
@@ -1,8 +1,42 @@
 package mk.ukim.finki.cinemania.ui.screens.profile
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import mk.ukim.finki.cinemania.domain.authentication.AuthenticationRepository
+import mk.ukim.finki.cinemania.domain.movie.MovieRepository
 
 @HiltViewModel
-class ProfileViewModel @Inject constructor() : ViewModel()
+class ProfileViewModel @Inject constructor(
+    authenticationRepository: AuthenticationRepository,
+    movieRepository: MovieRepository
+) : ViewModel() {
+
+    private val _stateFlow: MutableStateFlow<ProfileState?> = MutableStateFlow(null)
+    val stateFlow: StateFlow<ProfileState?> = _stateFlow
+
+    private val _loadingStateFlow: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val loadingStateFlow: StateFlow<Boolean> = _loadingStateFlow
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            _loadingStateFlow.value = true
+            val name = authenticationRepository.getCurrentUser()?.displayName.orEmpty()
+            // TODO: Implement logic for fetching actual recommendations. Displaying the popular movies for now.
+            val movieList = movieRepository.fetchPopularMovieList()
+            _stateFlow.value = ProfileState(
+                name = name,
+                watchedMovieRecommendationsName = "Inception",
+                watchedMovieRecommendations = movieList,
+                likedMovieRecommendationsName = "Titanic",
+                likedMovieRecommendations = movieList
+            )
+            _loadingStateFlow.value = false
+        }
+    }
+}
