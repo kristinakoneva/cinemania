295f09a9c6c982d32ae78d96d2434cc39996e4a4
Kristina Koneva <kristina.koneva@infinum.com>
2024-04-02 21:06:12 +0200

    Add data and domain logic for fetching movie details by id

---
+++ app/src/main/java/mk/ukim/finki/cinemania/data/api/models/MovieDetailsResource.kt
@@ -0,0 +1,28 @@
+package mk.ukim.finki.cinemania.data.api.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MovieDetailsResource(
+    @SerialName("id")
+    val id: Int,
+    @SerialName("title")
+    val title: String,
+    @SerialName("overview")
+    val overview: String,
+    @SerialName("poster_path")
+    val posterPath: String? = null,
+    @SerialName("release_date")
+    val releaseDate: String,
+    @SerialName("vote_average")
+    val rating: Double,
+    @SerialName("genres")
+    val genres: List<GenreResource>,
+    @SerialName("production_countries")
+    val productionCountries: List<ProductionCountryResource>,
+    @SerialName("spoken_languages")
+    val spokenLanguages: List<SpokenLanguageResource>,
+    @SerialName("runtime")
+    val runtime: Int
+)
